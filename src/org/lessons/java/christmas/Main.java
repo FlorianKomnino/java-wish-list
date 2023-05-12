5f70e47d8eef2689e36bbd2467e4bfec53456e1f
FlorianKomnino <florian.komnino@outlook.it>
2023-05-12 11:51:20 +0200

    completed wishList exercise

---
+++ src/org/lessons/java/christmas/Main.java
@@ -0,0 +1,39 @@
+package org.lessons.java.christmas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		List<String> wishList = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+
+		while(true) {
+			
+			System.out.println("Vuoi esprimere un desiderio?");
+			System.out.println("1 - si");
+			System.out.println("2 - no");
+			
+			int choice = sc.nextInt();
+			
+			while (choice<1 || choice>2) {
+				System.out.println("Scelta non disponibile. Inserisci una scelta valida");
+				continue;
+			}
+			if(choice == 1) {
+				System.out.println("Esprimi il tuo desiderio:");
+				sc.nextLine();
+				String wish = sc.nextLine();
+				wishList.add(wish);
+				System.out.println("Fin'ora hai espresso " + wishList.size() + " desideri");
+			} else if (choice == 2) {
+				break;
+			}
+
+		}
+		
+		System.out.println("Ecco la lista dei tuoi desideri");
+		System.out.println(wishList);
+	}
+}
